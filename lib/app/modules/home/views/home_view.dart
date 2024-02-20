b2d204efc1c69ba916ef110358f72f3128886d0d
Sifatsb <sifatsb1906@gmail.com>
2024-02-21 01:06:24 +0600

    done api call & initial project set up

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,6 +8,7 @@ import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,21 +17,22 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Obx(() => ListView.builder(
-        itemCount: controller.projects.length + 1,
-        itemBuilder: (context, index) {
-          if (index < controller.projects.length) {
-            return ListTile(
-              title: Text('${controller.projects[index].projectName}'),
-              subtitle: Text('${controller.projects[index].assignedEngineer}'),
-            );
-          } else if (controller.isLoading.value) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            return Container();
-          }
-        },
-        controller: controller.scrollController,
-      )),
+            itemCount: controller.projects.length + 1,
+            itemBuilder: (context, index) {
+              if (index < controller.projects.length) {
+                return ListTile(
+                  title: Text('${controller.projects[index].projectName}'),
+                  subtitle:
+                      Text('${controller.projects[index].assignedEngineer}'),
+                );
+              } else if (controller.isLoading.value) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return Container();
+              }
+            },
+            controller: controller.scrollController,
+          )),
     );
   }
 }
