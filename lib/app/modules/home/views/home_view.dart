918a8fc1a4d7fa8ee7b109bf86bc34829b8f7010
Sifatsb <sifatsb1906@gmail.com>
2024-02-21 01:39:57 +0600

    update ui

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -20,15 +20,55 @@ class HomeView extends GetView<HomeController> {
             itemCount: controller.projects.length + 1,
             itemBuilder: (context, index) {
               if (index < controller.projects.length) {
-                return ListTile(
-                  title: Text('${controller.projects[index].projectName}'),
-                  subtitle:
-                      Text('${controller.projects[index].assignedEngineer}'),
+                return Container(
+                  padding: const EdgeInsets.all(6),
+                  // margin: const EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                    color: index % 2 == 1 ? Colors.white : Colors.grey.shade200,
+                    
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(controller.projects[index].projectName ?? 'No Data'),
+                      Text(controller.projects[index].assignedEngineer ?? 'No Data'),
+                      // IconButton(onPressed: (){
+                      // }, icon: const Icon(Icons.more_vert_outlined))
+
+                      PopupMenuButton<int>(
+                        // padding: EdgeInsets.zero,
+                        color: Colors.white,
+                        iconColor: Colors.white,
+                        child:  const Icon(Icons.more_vert_outlined),
+                        itemBuilder: (context) => [
+                          PopupMenuItem(
+                            value: 1,
+                            onTap: (){},
+                            child:  Text("View".tr),
+                          ),
+                          const PopupMenuDivider(),
+                          PopupMenuItem(
+                            value: 2,
+                            onTap: (){},
+                            child:  Text("Add".tr),
+                          ),
+                          const PopupMenuDivider(),
+                          PopupMenuItem(
+                            value: 2,
+                            onTap: (){},
+                            child:  Text("Update".tr),
+                          ),
+                        ],
+                      )
+
+                    ],
+                  ),
                 );
               } else if (controller.isLoading.value) {
                 return const Center(child: CircularProgressIndicator());
               } else {
-                return Container();
+                return const Center(child: Text('No data available', style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),));
               }
             },
             controller: controller.scrollController,
